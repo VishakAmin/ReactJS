89ce407be7609c8534d7af7a6539b6b3bd90870c
VishakAmin <aminvishak@gmail.com>
2020-05-23 22:28:35 +0530

    course section

--- src/components/Cart/Coursecart.js
+++ src/components/Cart/Coursecart.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 import Heading from '../Reusable/Heading'
 import Img from 'gatsby-image'
 
+const getCaty = items => {
+    let holdeItems = items.map(items => {
+        return items.node.category
+
+    })
+    let holdCategories = new Set(holdeItems)
+    let categories = Array.from(holdCategories)
+    categories = ["all",  ...categories]
+    return categories
+}
+
+
 
 export default class Coursecart extends Component {
     constructor(props){
@@ -9,7 +21,25 @@ export default class Coursecart extends Component {
         this.state ={
             courses : props.courses.edges,
             mycourses : props.courses.edges,
+            mycategories: getCaty(props.courses.edges),
 
+        }                                                                                                                    
+    }
+
+    catyClicked = category => { 
+        let keepItsafe = [...this.state.courses]
+
+        if(category === 'all') {
+            this.setState(() => {
+                return{
+                    mycourses: keepItsafe
+                }
+            })
+        }else{
+            let holdme = keepItsafe.filter(({node}) => node.category === category)
+              this.setState(() =>{
+                  return {mycourses: holdme}
+              } )  
         }
     }
     render() {
@@ -18,13 +48,39 @@ export default class Coursecart extends Component {
         return (
             <section className = "py-5">
                 <div className="container">
-                    <Heading title = "Course" />
+                    <Heading title = "Courses" />
+                    <div className="row my-3">
+                        <div className="col-10 mx-auto text-center">
+                        {
+                            this.state.mycategories.map((category, index) => {
+                                return (
+                                    <button type="button"
+                                    className ="btn btn-info m-3 px-3"
+                                    key={index}
+                                    onClick={() =>{
+                                        this.catyClicked(category)    
+                                    }}
+
+
+                                    >
+                                    {category}
+                                
+                                    
+                                    </button>
+                                )           
+
+
+                            })
+                        }
+                        
+                        </div>
+                    </div>
                     <div className = "row">
                         { this.state.mycourses.map( ({node}) => {
 
                             return(
                                 <div
-                                key= {node.id} className = "col-11 col-md-6 g-flex mx-auto d-flex justify-content-between">
+                                key= {node.id} className = "col-11 col-md-6 g-flex mx-auto d-flex justify-content-between mt-4">
                                 <Img fixed = {node.image.fixed} />
                                 <div className="flex-grow-1 px-3 ">
                                     <div className="d-flex justify-content-between">
--- src/components/Reusable/DualInfoblock.js
+++ src/components/Reusable/DualInfoblock.js
@@ -20,7 +20,7 @@ export default function DualInfoblock({heading}) {
                  <img src="https://images.pexels.com/photos/3561340/pexels-photo-3561340.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" className="card-img-top" alt="Images goes here"/>
                  <div className="card-body">
                    <h5 className="card-title text-success">Just Click photo</h5>
-                   <p className="card-text text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea dolor debitis quae, adipisci, quia excepturi nulla rem deserunt eum doloribus ex voluptatibus assumenda id. Perferendis necessitatibus fugiat odio natus ad.</p>
+                   <p className="card-text text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea dolor debitis quae, adipisci, quia excepturi nulla .</p>
                   <Link to = "/about/">
                    <button className="btn btn-warning btn-block">
                    {heading}
